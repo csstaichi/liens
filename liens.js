55499d971f1cb590491005860d4b0c12f1a567bd
csstaichi <cssmooreatahiti@gmail.com>
2026-08-16 19:07:04 -1000

    Add files via upload

--- liens.js
+++ liens.js
@@ -22,7 +22,7 @@
    valeur doit exister. À mettre à jour à CHAQUE livraison, en même temps
    que le nom du zip, sinon la page ment sur ce qu'elle est.
    --------------------------------------------------------------------- */
-const VERSION = "00.29.00";
+const VERSION = "00.30.00";
 const VERSION_DATE = "16 août 2026";
 
 
